7ec256bc003ccea44902b5dab8321805e27dda65
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-07-28 18:46:00 +0100

    pass jobid as params

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const axios = require('axios');
 require('dotenv').config();
 
-const apiUrl = 'https://developers.checkphish.ai/api/neo/scan/status';
-const apiKey = process.env.API_KEY;
-const jobID = '853e0424-ac1c-4bcd-bd96-ee5eb316acfc';
+function checkJobStatus(jobID) {
+  const apiUrl = 'https://developers.checkphish.ai/api/neo/scan/status';
+  const apiKey = process.env.API_KEY;
 
-const data = {
-  apiKey: apiKey,
-  jobID: jobID,
-  insights: true
-};
+  const data = {
+    apiKey: apiKey,
+    jobID: jobID,
+    insights: true
+  };
 
-axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
-  .then(response => {
-    console.log('Response:', response.data);
-  })
-  .catch(error => {
-    console.error('Error:', error.message);
-});
+  axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
+    .then(response => {
+      console.log('Response:', response.data);
+    })
+    .catch(error => {
+      console.error('Error:', error.message);
+    });
+}
+
+module.exports = checkJobStatus; // Export the function
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 require('dotenv').config();
+const app = require('./app'); // Import app.js
 
 const apiUrl = 'https://developers.checkphish.ai/api/neo/scan';
 const apiKey = process.env.API_KEY;
-const urlToScan = 'https://www.wariate.com/';
+const urlToScan = 'https://www.google.com/';
 
 const data = {
   apiKey: apiKey,
@@ -15,6 +16,8 @@ const data = {
 axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
     .then(response => {
         console.log('Response:', response.data);
+        const jobID = response.data.jobID; // Get the jobID from the response
+        app(jobID); // Call app.js and pass the jobID as a parameter
     })
     .catch(error => {
         console.error('Error:', error.message);
