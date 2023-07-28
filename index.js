2ffce2b698ad0859e87edebe11ccb2e3d6379ab2
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-07-28 21:31:41 +0100

    get user jodId and check validation

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
 const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
 require('dotenv').config();
-const app = require('./app'); // Import app.js
 
-const apiUrl = 'https://developers.checkphish.ai/api/neo/scan';
-const apiKey = process.env.API_KEY;
-const urlToScan = 'https://www.google.com/';
+const app = express();
+const port = 8000;
 
-const data = {
-  apiKey: apiKey,
-  urlInfo: {
-    url: urlToScan
-  }
-};
+function checkJobStatus(jobID, res) {
+  const apiUrl = 'https://developers.checkphish.ai/api/neo/scan/status';
+  const apiKey = process.env.API_KEY;
 
-axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
+  const data = {
+    apiKey: apiKey,
+    jobID: jobID,
+    insights: true
+  };
+
+  axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
     .then(response => {
-        console.log('Response:', response.data);
-        const jobID = response.data.jobID; // Get the jobID from the response
-        app(jobID); // Call app.js and pass the jobID as a parameter
+      console.log('Response:', response.data);
+      res.status(200).json(response.data); // Send the response.data back to Postman
     })
     .catch(error => {
-        console.error('Error:', error.message);
+      console.error('Error:', error.message);
+      res.status(500).json({ message: 'Error checking URL scan status' });
+    });
+}
+
+app.use(bodyParser.json());
+
+app.post('/scan-url', (req, res) => {
+  const apiUrl = 'https://developers.checkphish.ai/api/neo/scan';
+  const apiKey = process.env.API_KEY;
+  const urlToScan = req.body.urlToScan; // Get the urlToScan from the POST request body
+
+  const data = {
+    apiKey: apiKey,
+    urlInfo: {
+      url: urlToScan
+    }
+  };
+
+  axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
+    .then(response => {
+      console.log('Response:', response.data);
+      const jobID = response.data.jobID; // Get the jobID from the response
+      checkJobStatus(jobID, res); // Call checkJobStatus function and pass the jobID and response object
+    })
+    .catch(error => {
+      console.error('Error:', error.message);
+      res.status(500).json({ message: 'Error starting URL scan' });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
