82cc4b056663f3ebce17dbafe0daf3c3ff4a10d3
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-08-28 12:23:46 +0100

    -[] added express to static -[] added status

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
+app.use(express.static('public')); // Serve your HTML and frontend scripts
 app.use(cors());
 const port = 8000;
+
+// Target the HTML element where you want to display the data
+// const apiDataElement = document.getElementById('apiData');
+// console.log(apiDataElement, 'here');
 
 function checkJobStatus(jobID, res) {
   const apiUrl = 'https://developers.checkphish.ai/api/neo/scan/status';
@@ -20,7 +25,7 @@ function checkJobStatus(jobID, res) {
 
   axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
     .then(response => {
-      console.log('Response:', response.data);
+      console.log('response:', response.data);
       res.status(200).json(response.data); // Send the response.data back to Postman
     })
     .catch(error => {
@@ -35,6 +40,7 @@ app.post('/scan-url', (req, res) => {
   const apiUrl = 'https://developers.checkphish.ai/api/neo/scan';
   const apiKey = process.env.API_KEY;
   const urlToScan = req.body.urlToScan; // Get the urlToScan from the POST request body
+  // console.log(urlToScan);
 
   const data = {
     apiKey: apiKey,
@@ -46,6 +52,8 @@ app.post('/scan-url', (req, res) => {
   axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
     .then(response => {
       console.log('Response:', response.data);
+      const status = response.data.status;
+      // apiDataElement.textContent = JSON.stringify(status);
       const jobID = response.data.jobID; // Get the jobID from the response
       checkJobStatus(jobID, res); // Call checkJobStatus function and pass the jobID and response object
     })
