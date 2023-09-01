2900794989c365a3a65ce6507df9cb750bb03d36
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-09-01 08:09:35 +0100

    -[] pass express as json -[] added post url req -[] serve resp

--- index.js
+++ index.js
@@ -5,63 +5,82 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(express.static('public')); // Serve your HTML and frontend scripts
 app.use(cors());
-const port = 8000;
+app.use(express.json());
+const port = 9000;
 
 // Target the HTML element where you want to display the data
 // const apiDataElement = document.getElementById('apiData');
 // console.log(apiDataElement, 'here');
 
-function checkJobStatus(jobID, res) {
-  const apiUrl = 'https://developers.checkphish.ai/api/neo/scan/status';
-  const apiKey = process.env.API_KEY;
+app.post('/scan-url', async (req, res) => {
+  const { urlToScan } = req.body;
+  try {
+    const apiUrl = 'https://developers.checkphish.ai/api/neo/scan';
+    const apiKey = process.env.API_KEY;
+    console.log(urlToScan);
 
-  const data = {
-    apiKey: apiKey,
-    jobID: jobID,
-    insights: true
-  };
+    const data = {
+      apiKey: apiKey,
+      urlInfo: {
+        url: urlToScan
+      }
+    };
 
-  axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
-    .then(response => {
-      console.log('response:', response.data);
-      res.status(200).json(response.data); // Send the response.data back to Postman
-    })
-    .catch(error => {
-      console.error('Error:', error.message);
-      res.status(500).json({ message: 'Error checking URL scan status' });
-    });
-}
+    const response = await axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } });
+    console.log('Response:', response.data);
 
-app.use(bodyParser.json());
+    const status = response.data.status;
+    const jobID = response.data.jobID;
 
-app.post('/scan-url', (req, res) => {
-  const apiUrl = 'https://developers.checkphish.ai/api/neo/scan';
-  const apiKey = process.env.API_KEY;
-  const urlToScan = req.body.urlToScan; // Get the urlToScan from the POST request body
-  // console.log(urlToScan);
+    // Send the first response
+    // res.status(200).json({ message: 'Data received', data: response.data });
 
-  const data = {
-    apiKey: apiKey,
-    urlInfo: {
-      url: urlToScan
-    }
-  };
-
-  axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } })
-    .then(response => {
-      console.log('Response:', response.data);
-      const status = response.data.status;
-      // apiDataElement.textContent = JSON.stringify(status);
-      const jobID = response.data.jobID; // Get the jobID from the response
-      checkJobStatus(jobID, res); // Call checkJobStatus function and pass the jobID and response object
-    })
-    .catch(error => {
-      console.error('Error:', error.message);
-      res.status(500).json({ message: 'Error starting URL scan' });
-    });
+    // Check job status and send the second response
+    const jobStatusResponse = await checkJobStatus(jobID);
+    // Send the second response here
+    res.status(200).json(jobStatusResponse);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ message: 'Error starting URL scan' });
+  }
 });
+
+
+app.get('/job-status/:jobID', async (req, res) => {
+  const { jobID } = req.params;
+  console.log(`Received request for job status with ID: ${jobID}`);
+  try {
+    const jobStatus = await checkJobStatus(jobID);
+    console.log('Job Status:', jobStatus);
+    res.status(200).json(jobStatus);
+  } catch (error) {
+    console.error('Error:', error.message);
+    res.status(500).json({ message: 'Error fetching job status' });
+  }
+});
+
+
+async function checkJobStatus(jobID) {
+  try {
+    const apiUrl = 'https://developers.checkphish.ai/api/neo/scan/status';
+    const apiKey = process.env.API_KEY;
+
+    const data = {
+      apiKey: apiKey,
+      jobID: jobID,
+      insights: true
+    };
+
+    const response = await axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } });
+    console.log('Job Status Response:', response.data);
+
+    return response.data;
+  } catch (error) {
+    console.error('Error:', error.message);
+    throw error; // Re-throw the error to handle it in the main route handler
+  }
+}
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
