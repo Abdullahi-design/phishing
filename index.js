0cb9109f76352ca3b6fa9d3504760d35472c6db0
Abdullahi-design <abdullahismohd3000@gmail.com>
2023-09-02 14:27:53 +0100

    commented

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
-const axios = require('axios');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-require('dotenv').config();
+const axios = require('axios'); // for request send and accept
+const express = require('express'); // responsible for starting up server
+const cors = require('cors'); // responsible for gateway security
+require('dotenv').config(); // fetches api from  enviroment variable
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-const port = 9000;
+const app = express(); // nodejs sever decleration
+app.use(cors()); // declearing cors
+app.use(express.json()); // pass as an obj format
+const port = 9000; //local host port
 
-// Target the HTML element where you want to display the data
-// const apiDataElement = document.getElementById('apiData');
-// console.log(apiDataElement, 'here');
 
 app.post('/scan-url', async (req, res) => {
-  const { urlToScan } = req.body;
+  const { urlToScan } = req.body; // getting the url from frontend
   try {
-    const apiUrl = 'https://developers.checkphish.ai/api/neo/scan';
-    const apiKey = process.env.API_KEY;
+    const apiUrl = 'https://developers.checkphish.ai/api/neo/scan'; //checkphish api url endpoint
+    const apiKey = process.env.API_KEY; // getting the api key from .env
     console.log(urlToScan);
 
+    // defining the data to be sent to the endpoint url
     const data = {
       apiKey: apiKey,
       urlInfo: {
@@ -27,14 +24,13 @@ app.post('/scan-url', async (req, res) => {
       }
     };
 
+    // await keeps the req busy until it gets a response
+    // is a post request bcoz it sends data to the endpoint url
+    // setting content type to object format (json)
     const response = await axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } });
     console.log('Response:', response.data);
 
-    const status = response.data.status;
-    const jobID = response.data.jobID;
-
-    // Send the first response
-    // res.status(200).json({ message: 'Data received', data: response.data });
+    const jobID = response.data.jobID; // get the job id from response.data
 
     // Check job status and send the second response
     const jobStatusResponse = await checkJobStatus(jobID);
@@ -46,32 +42,36 @@ app.post('/scan-url', async (req, res) => {
   }
 });
 
-
+// validation from frontend
 app.get('/job-status/:jobID', async (req, res) => {
-  const { jobID } = req.params;
+  const { jobID } = req.params; // passing job id as parameter
   console.log(`Received request for job status with ID: ${jobID}`);
   try {
-    const jobStatus = await checkJobStatus(jobID);
+    const jobStatus = await checkJobStatus(jobID); // pasing job id 
     console.log('Job Status:', jobStatus);
-    res.status(200).json(jobStatus);
+    res.status(200).json(jobStatus); // sending jobstatus to frontend
   } catch (error) {
     console.error('Error:', error.message);
     res.status(500).json({ message: 'Error fetching job status' });
   }
 });
 
-
+// async is a waiting function
 async function checkJobStatus(jobID) {
   try {
     const apiUrl = 'https://developers.checkphish.ai/api/neo/scan/status';
     const apiKey = process.env.API_KEY;
 
+    // defining the data to be sent to the endpoint url
     const data = {
       apiKey: apiKey,
-      jobID: jobID,
-      insights: true
+      jobID: jobID, // pass job id
+      insights: true // link to checkphish api 
     };
 
+    // await keeps the req busy until it gets a response
+    // is a post request bcoz it sends data to the endpoint url
+    // setting content type to object format (json)
     const response = await axios.post(apiUrl, data, { headers: { 'Content-Type': 'application/json' } });
     console.log('Job Status Response:', response.data);
 
